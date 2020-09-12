d36267041ba03ffd679729264d1be2f1ddc26a70
Leonid <lzimin05@mail.ru>
2020-09-12 13:17:25 +0300

    Add arrays_copy

---
+++ arrays_copy.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void fill(int a[], int size) {
+	srand(time(NULL));
+	for (int i = 0; i < size; i++) {
+		a[i] = rand() % 100 + 1; 
+	}
+}
+
+void printArray(int a[], int size) {
+	// [1, 2, 3, 4, 5, 6]
+	printf("[");
+	for (int i = 0; i < size; i++) {
+		if (i == size -1) {
+			printf("%i", a[i]); 
+		} else {
+			printf("%i, ", a[i]);
+		}
+	}
+	printf("]\n");
+}
+
+int main(int argc, char** argv) {
+
+	int n;
+	scanf("%i", &n);
+	
+	int* a = (int*) malloc(sizeof(int) * n);
+	
+	fill(a, n);
+	printArray(a, n);
+	
+	free(a);
+	return 0;
+}
