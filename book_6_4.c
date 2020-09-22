6703ceceaadab74064378ed23ec4cb5d061bb1fc
Maxim Alov <alovmax@yandex.ru>
2020-09-22 17:05:07 +0300

    Add book_6_4

---
+++ book_6_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    int x, y;
+    scanf("%i %i", &x, &y);
+    if (x > y) {
+  	printf("True\n");
+    } else {
+  	printf("False\n");
+    }
+
+    return 0;
+}
