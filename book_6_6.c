93d7f36222973b523e301a1e54ba6e3ece59dee6
lzimin05 <lzimin05@mail.ru>
2020-09-22 20:48:08 +0300

    Add book_6_6

---
+++ book_6_6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    char a = 'a';
+    int size1 = 0; 
+    int size2 = 0;
+    while (a != '\n') {
+        scanf("%c", &a);
+    	size1++;
+    }
+    a = 'a';
+    while (a != '\n') {
+	scanf("%c", &a);
+    	size2++;
+    }
+
+    if (size1 != size2) {
+	printf("True\n");
+    } else {
+	printf("False\n");
+    }
+
+    return 0;
+}
+
