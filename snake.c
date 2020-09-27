7a537bd674be457c28717f54cc5b7f7c32d88334
lzimin05 <lzimin05@mail.ru>
2020-09-27 17:22:32 +0300

    Add tail for snake

--- snake.c
+++ snake.c
@@ -27,30 +27,42 @@ int main(int argc, char** argv) {
     int x1 = -1;
     int y1 = 0;
     int score = 0;
+    int key = -1;
+    int size = 0;
     while (true) {  
+	system("clear");	
+	printf("%c", key);	
 	srand(time(NULL));
 	if (x1 == -1) {
 	    x1 = rand() % (N - 2) + 1; 	//яблоко
 	    y1 = rand() % (M - 2) + 1;
 	}
 	char a[N][M];
-        int key = -1;
 	if (x1 == x && y1 == y) {
 	    score++;
 	    a[x1][y1] = '0';		//если змейка достигла яблока	
 	    x1 = -1;
+	    size++;
 	}
-        system("clear");
     	for(int i = 0; i < N; i++) {
 	    for(int j = 0; j < M; j++) {
 	    	if (i == 0 || j == 0 || i == N - 1 || j == M - 1) {
 	            a[i][j] = '#';
-	        } else {						//строительство поля
+	        } else {				//строительство поля
 	       	    a[i][j] = ' ';
+		    
 	        }
 	    	if (i == x && j == y) { 
-		    a[i][j] = '@';				//голова змейки
+		    a[i][j] = '@';					//голова змейки						
 	    	}
+		if (score > 0) {
+		    for(int i = 1; i <= size; i++) {
+		      	if (key == 'w') a[x+i][y] = '@';
+	    	     	if (key == 's') a[x-i][y] = '@';
+	    	    	if (key == 'a') a[x][y+i] = '@';
+	    	    	if (key == 'd') a[x][y-i] = '@';
+		    }
+		}
 		if (i == x1 && j == y1) {
 		    a[i][j] = '*';					//размещение яблок
 		}
@@ -58,17 +70,16 @@ int main(int argc, char** argv) {
 	    }
    	    printf("\n");
     	}
-	printf("score: %i\n", score);	//кол-во очков
+	printf("score: %i\n", score);	//кол-во очков	
 	if (x == 0 || x == N - 1 || y == 0 || y == M - 1) {
 	    break;						//game over
 	}
-	key = getch();
+	key = getch();	
 	if (key == 'w') x--;
 	if (key == 's') x++;
 	if (key == 'd') y++;				//назначение клавиш
 	if (key == 'a') y--;
 	if (key == 'e') break;
-	system("clear");
     }
 
     return 0;
