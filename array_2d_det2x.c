aefd68f9588c94b869b2d4e7e2dba74325ea5635
lzimin05 <lzimin05@mail.ru>
2020-10-10 15:10:44 +0300

    Add array_2d_det2x

---
+++ array_2d_det2x.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void fill(int** a, int size) {
+    srand(time(NULL) * (int) a);
+    for(int i = 0; i < size; i++) {
+	for(int g = 0; g < size; g++) {	
+	    a[i][g] = rand() % 90 + 10;
+	}
+    }
+}
+
+void printArray(int** a, int size) {
+    for(int i = 0; i < size; i++) {
+	printf("[");
+	for(int g = 0; g < size; g++) {
+	    if(g == size -1) {
+		printf("%i]\n", a[i][g]);
+	    } else {
+		printf("%i, ", a[i][g]);
+	    }
+	}
+    }
+}
+
+int det(int** a, int size) {
+    return a[0][0] * a[1][1] - a[0][1] * a[1][0];
+}
+
+int main(int argc, char** argv) {
+
+    int size = 2;
+    int** a = (int**) malloc(sizeof(int) * size*size);
+    for(int i = 0; i < size; i++) {
+	a[i] = (int*) malloc(sizeof(int) * size);
+    }
+    fill(a, size);
+    printArray(a, size);
+    printf("\n\n");
+    int d = det(a, size);
+    printf("%i\n", d);
+    for(int i = 0; i < size; i++) {
+	free(a[i]);
+    }
+    free(a);
+    return 0;
+}
