4a211e4e99d08348b6121361b74f4dc7b7c5a180
lzimin05 <lzimin05@mail.ru>
2020-10-18 14:07:09 +0300

    Add easy_17

---
+++ easy_17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int size = 0;
+    char c[size];
+    do {
+	scanf("%c", &c[size]);    	
+	size++;
+    } while(c[size-1] != '\n');
+    size--;
+    printf("%i\n", size);
+    int k;
+    scanf("%i", &k);
+    printf("\nk = %c\n", c[k]);
+    return 0;
+}
